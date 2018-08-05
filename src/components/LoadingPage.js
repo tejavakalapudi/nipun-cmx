8d736145c5296d4c7f60b830eef076ffa1be7383
Raviteja Vakalapudi <tejavakalapudi@gmail.com>
2018-08-05 12:40:15 +0530

    Modified loader icon

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import LoaderIcon from "../../public/images/loader.svg";
 
 const LoadingPage = ( props ) => ( 
 
     <div className="loader" style={{ height: props.height || "" , width: props.width || "" }}>
-        <img className="loader__image" src= "/images/loader.gif" />
+        <img className="loader__image" src= { LoaderIcon } />
     </div>
         
 );
